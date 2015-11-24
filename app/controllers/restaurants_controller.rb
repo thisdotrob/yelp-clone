6ec628d5dcfca4e8f142ed9b5b080fcc5fafd88f
Mari-Ann Meling <mariann013@gmail.com>
2015-11-24 15:38:01 +0000

    new restaurant

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,19 @@
+class RestaurantsController < ApplicationController
+  # def index
+  #   raise "Hello from the index action"
+  # end
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def create
+    Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
+  end
+
+end
