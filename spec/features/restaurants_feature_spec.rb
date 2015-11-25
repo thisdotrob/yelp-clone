1a2faab808398beb7b25abc598f16d1cfafade4a
George Maddocks <georgemaddocks@yahoo.co.uk>
2015-11-25 15:53:06 +0000

    added reviews

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -1,6 +1,4 @@
-require 'rails_helper'
-
-  feature 'restaurants' do
+feature 'restaurants' do
     context 'no restaurants have been added' do
       scenario ' should display a prompt to add a restaurant' do
         visit '/restaurants'
