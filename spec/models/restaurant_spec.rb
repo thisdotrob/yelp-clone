13bdefffc357ebb4768c9f11f9f3f43cf7d2f324
George Maddocks <georgemaddocks@yahoo.co.uk>
2015-11-25 17:13:08 +0000

    tests for name length validation

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,3 +1,9 @@
 describe Restaurant, type: :model do
   it { is_expected.to have_many :reviews }
+
+  it 'is not valid with a name of fewer than three characters' do
+    restaurant = Restaurant.new(name: 'kf')
+    expect(restaurant).to have(1).error_on(:name)
+    expect(restaurant).not_to be_valid
+  end
 end
