71a356560c5bc257ce150f2c6560751ad7637821
thisdotrob <thisdotrob@gmail.com>
2015-11-26 11:52:19 +0000

    Add facebook authentication

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 
 gem 'devise'
 
+gem 'omniauth-facebook'
+
 gem 'haml'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
+
   root to: "restaurants#index"
 
   resources :restaurants do
